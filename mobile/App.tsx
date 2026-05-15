ef68556df42b48b05cdbd7a9e999d5c2d6ec3750
jcvch05 <jcarlos.vilaseca@outlook.com>
2026-05-15 16:13:08 -0400

    chore: sync local config changes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AppNavigator } from './src/navigation/AppNavigator';
 import { ThemeProvider } from './src/context/ThemeContext';
 export default function App() {
   return (
-    <ThemeProvider>
-      <StatusBar style="light" />
-      <AppNavigator />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider>
+        <StatusBar style="light" />
+        <AppNavigator />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
